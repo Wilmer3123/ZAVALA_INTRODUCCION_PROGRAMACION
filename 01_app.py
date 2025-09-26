dca4a077c6b8dbed53a988982bd3a38e051010a0
Wilmer3123 <i202518242@cibertec.edu.pe>
2025-09-26 06:21:22 +0000

    Ejercicios de programacion

---
+++ 01_app.py
@@ -0,0 +1,7 @@
+# Operaciones matemáticas básicas
+
+# Suma
+print(4+5)
+
+# Resta
+print(100-45)
---
+++ 04_app.py
@@ -0,0 +1,11 @@
+# Calcular el 18% de IGV a partir de un monto de pago
+
+# Inicio: Definir o crear las variables
+monto_pago = 600
+igv = 0.18
+
+# Proceso: Realizar las operaciones de cálculo
+monto_a_pagar = monto_pago + (monto_pago * igv)
+
+# Salida: Vizualización de resultados
+print(monto_a_pagar)
